fa763ca836e6f9d02c9e9eb6672b651268ac4ef2
mediexaminfo <mediexaminfo@gmail.com>
2021-08-22 23:20:31 +0600

    huha

---
+++ src/routes/__error.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+	export function load({ error, status }) {
+		return {
+			props: {
+				status
+			}
+		};
+	}
+</script>
+
+<script>
+	export let status;
+</script>
+
+<svelte:head>
+	<title>Hmm... An error occurred.</title>
+</svelte:head>
+
+{#if status === 404}
+	<h1 class="text-2xl text-gray-700">Not found. What exactly are you looking for?</h1>
+{:else}
+	<h1 class="text-2xl text-gray-700">Oopss. An error occurred!</h1>
+{/if}
